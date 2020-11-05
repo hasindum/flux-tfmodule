f5560001730e5e3ecb90a1b451a8cc57aadb7cb0
Hush <hasalertmon@gmail.com>
2020-11-06 00:28:34 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,23 +2,4 @@ terraform {
 
   required_version = ">= 0.12"
 
-  //  required_providers {
-  //    aws = {
-  //      source  = "hashicorp/aws"
-  //      version = "3.6.0"
-  //    }
-  //    helm = {
-  //      source  = "hashicorp/helm"
-  //      version = "1.3.0"
-  //    }
-  //    kubernetes = {
-  //      source  = "hashicorp/kubernetes"
-  //      version = "1.13.2"
-  //    }
-  //    template = {
-  //      source  = "hashicorp/template"
-  //      version = "2.1.2"
-  //    }
-  //  }
-
 }
